08fe6556ef771122f1170210f7c21a12598773f1
YAPO-AKO <ext-Adou.YAPO-AKO@FromEarthToLife.net>
2020-03-29 19:43:16 +0200

    Version 1

---
+++ ENV/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ext-adou.YAPO-AKO\Documents\PROJETS\DJANGO\dutch\ENV\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
